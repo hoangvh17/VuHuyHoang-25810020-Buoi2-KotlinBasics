c92c0e12a675b7599ce76f7108ad7610c8867939
Vu Huy Hoang <149637745+hoangvh17@users.noreply.github.com>
2026-09-23 23:12:24 +0700

    Buoi02 Bai02 gioi thieu ban than

---
+++ Bai02_GioiThieuBanThan.kt
@@ -0,0 +1,10 @@
+// Vũ Huy Hoàng - 25810020
+fun main() {
+    val fullName = "Vu Huy Hoang"
+    val birthYear = 2004
+    val currentYear = 2026
+    val age = currentYear - birthYear
+    
+    println("Hế luu Thầy, em tên là $fullName, năm sinh $birthYear, hiện tại $age tuổi.")
+    
+}
