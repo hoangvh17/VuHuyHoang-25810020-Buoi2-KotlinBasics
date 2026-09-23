ea53a5d573a2b6a78d53d67fdace62b07aac232f
Vu Huy Hoang <lahoangchuai@gmail.com>
2026-09-23 22:29:36 +0700

    Add kotlin exercise

---
+++ app/src/main/java/Bai01_TinhTienMuaHang.kt
@@ -0,0 +1,16 @@
+
+fun cal() {
+    val qty: Int = 5
+    val price: Double = 25.5
+    val fee = 0.08
+    val result: Double = price * qty.toDouble()
+    print(result)
+}
+
+fun main(){
+    cal()
+}
+
+
+
+
