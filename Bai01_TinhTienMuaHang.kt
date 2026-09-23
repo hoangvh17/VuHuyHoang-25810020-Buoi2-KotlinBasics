87b1989d060bb0185f359487057e4c487d6df4bd
Vu Huy Hoang <149637745+hoangvh17@users.noreply.github.com>
2026-09-23 22:59:55 +0700

    Buoi02 Bai01 tinh tien mua hang

---
+++ Bai01_TinhTienMuaHang.kt
@@ -0,0 +1,15 @@
+// Vũ Huy Hoàng - 25810020
+// Buoi02 Bai01 tinh tien mua hang
+
+fun cal() {
+    val qty: Int = 5
+    val price: Double = 25.5
+    val fee = 0.08
+    val result: Double = price * qty.toDouble()
+    print(result)
+}
+
+fun main() {
+    cal()
+}
+
