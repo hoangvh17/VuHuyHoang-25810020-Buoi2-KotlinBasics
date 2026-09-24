19560afe5815d3bc82da804767a15d7722c2cfe8
Vu Huy Hoang <149637745+hoangvh17@users.noreply.github.com>
2026-09-24 21:26:43 +0700

    Buoi02 Bai07 dem nguoc va lap co dinh

---
+++ Bai07_WhileVaRepeat.kt
@@ -0,0 +1,14 @@
+// Vũ Huy Hoàng - 25810020
+fun main() {
+    
+    var num:Int = 10
+    while(num>0){
+        println("$num")
+        num--
+    }
+    repeat(5){
+        println("boi vi em la")
+    }
+    
+    // dung while khi chua biet so lan lap va repeat khi da biet so lan lap
+}
